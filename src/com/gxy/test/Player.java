3ff632e32d1858a619c58821b57776b046a9244c
guoxingyang <guoxingyang@126.com>
2016-10-29 20:23:00 +0800

    玩家

---
+++ src/com/gxy/test/Player.java
@@ -0,0 +1,21 @@
+package com.gxy.test;
+
+public class Player {
+
+	private int id;			//Id
+	private String name;	//Ãû×Ö
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
